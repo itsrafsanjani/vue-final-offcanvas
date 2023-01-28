41a9da04ca135889adcd81c14a8f3265349e078b
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2023-01-28 22:51:47 +0600

    feat: demo complete for vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Offcanvas v-model="showOffcanvas">
+    <template #header>
+      <span>
+        Hello World
+      </span>
+    </template>
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+  </Offcanvas>
+  <button @click="toggleOffcanvas" class="hover:bg-gray-200">Toggle Offcanvas</button>
 </template>
+
+<script>
+import Offcanvas from 'vue-final-offcanvas'
+export default {
+  components: {
+    Offcanvas
+  },
+
+  data() {
+    return {
+      showOffcanvas: false
+    }
+  },
+
+  methods: {
+    toggleOffcanvas() {
+      this.showOffcanvas = !this.showOffcanvas
+    }
+  }
+}
+</script>
 
 <style scoped>
 .logo {
@@ -20,9 +44,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
