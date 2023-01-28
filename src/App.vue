b5489329c3bb413001719149c6e52f03c1600a17
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2023-01-28 22:58:32 +0600

    update: demo content

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,18 @@
         Hello World
       </span>
     </template>
-    <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
+    <section v-for="i in 20" :key="i">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, itaque dolore! Aut non pariatur fugiat ad.
+        Expedita distinctio nostrum minus ducimus temporibus non laudantium quae tempore similique nisi reiciendis
+        doloremque neque molestiae quos itaque vel ipsum magni facere, rem, obcaecati, accusamus rerum repellat impedit
+        nesciunt! Non magni consequatur unde a saepe. Assumenda maxime dolores animi modi beatae officia fugiat
+        molestias provident amet veritatis earum accusamus recusandae repudiandae rem reprehenderit quia sit cumque a,
+        adipisci laudantium! Quisquam, doloribus minima? Cupiditate pariatur asperiores amet error culpa magni, quam
+        saepe fugit itaque modi. Odit voluptatum aspernatur esse alias quisquam dicta dolore incidunt inventore.
+      </p>
+      <hr class="my-3">
+    </section>
   </Offcanvas>
   <button @click="toggleOffcanvas" class="hover:bg-gray-200">Toggle Offcanvas</button>
 </template>
@@ -37,19 +41,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
